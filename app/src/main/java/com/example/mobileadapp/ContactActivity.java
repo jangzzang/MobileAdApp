d3650f2c61e06c1bb0736c031d24aa04a1d772d2
5mm-hun <dnsl9876@nate.com>
2020-05-22 15:03:50 +0900

    기모띠

--- app/src/main/java/com/example/mobileadapp/ContactActivity.java
+++ app/src/main/java/com/example/mobileadapp/ContactActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactActivity extends AppCompatActivity {
-
+    private Button btn_to_googleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
+        btn_to_googleMap = (Button)findViewById(R.id.btn_googleMap);
+        btn_to_googleMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ContactMapActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        Intent intent = new Intent(getApplicationContext(),ContactMapActivity.class);
-        startActivity(intent);
     }
 }
