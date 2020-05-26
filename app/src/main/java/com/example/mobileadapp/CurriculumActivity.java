b6ec0506b8553968696636d33c55b439c6a6feb5
5mm-hun <dnsl9876@nate.com>
2020-05-26 17:30:54 +0900

    sexnj

--- app/src/main/java/com/example/mobileadapp/CurriculumActivity.java
+++ app/src/main/java/com/example/mobileadapp/CurriculumActivity.java
@@ -2,13 +2,15 @@ package com.example.mobileadapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class CurriculumActivity extends AppCompatActivity {
     private ImageView iv_curri_back;
-
+    private Button btn_first;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,15 @@ public class CurriculumActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        btn_first = (Button)findViewById(R.id.btn_first_grade);
+        btn_first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),FirstGradeActivity.class);
+                startActivity(intent);
             }
         });
     }
