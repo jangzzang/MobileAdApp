ff6c98176cb89bd8162b51de43574e72c8d68dc8
5mm-hun <dnsl9876@nate.com>
2020-05-26 16:28:11 +0900

    sexxy

---
+++ app/src/main/java/com/example/mobileadapp/FirstGradeActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstGradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_grade);
+    }
+}
